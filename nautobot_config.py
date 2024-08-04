2a943e7e5224e1dde52377a3fdac16d483d683ab
Xander Harris <xandertheharris@gmail.com>
2024-08-04 09:53:04 -0700

    Update config
    Closes #7

--- nautobot_config.py
+++ nautobot_config.py
@@ -6,6 +6,7 @@ from loguru import logger
 
 from nautobot.core.settings import ALLOWED_URL_SCHEMES
 from nautobot.core.settings import AUTHENTICATION_BACKENDS
+from nautobot.core.settings import CACHEOPS_DEFAULTS
 from nautobot.core.settings import CACHEOPS_ENABLED
 from nautobot.core.settings import CELERY_BROKER_URL
 # from nautobot.core.settings import CELERY_BEAT_HEARTBEAT_FILE
@@ -30,11 +31,11 @@ from nautobot.core.settings_funcs import parse_redis_connection
 #                       #
 #########################
 logger.debug(f'allowed url schemes: {ALLOWED_URL_SCHEMES}')
+logger.debug(f'cache ops defaults: {CACHEOPS_DEFAULTS}')
 logger.debug(f'cache ops enabled: {CACHEOPS_ENABLED}')
 logger.debug(f'celery broker url: {CELERY_BROKER_URL}')
 logger.debug(f'celery task default queue: {CELERY_TASK_DEFAULT_QUEUE}')
 logger.debug(f'rq queues: {RQ_QUEUES}')
-logger.debug(f'cache ops enabled: {CACHEOPS_ENABLED}')
 # This is a list of valid fully-qualified domain names (FQDNs) for the Nautobot
 # server. Nautobot will not permit write
 # access to the server via any other hostnames. The first FQDN in the list will
