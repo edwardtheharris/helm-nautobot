fb0d92b6e99c853f23ac1ebf1c6d2be41b5d2dc6
Xander Harris <xandertheharris@gmail.com>
2024-08-08 11:40:53 -0700

    Update statefulset config
    Closes #21

--- config/nautobot.init.sh
+++ config/nautobot.init.sh
@@ -9,3 +9,5 @@ sudo -u nautobot python -m venv /opt/nautobot
 sudo -u nautobot /opt/nautobot/bin/pip install -U pip
 
 sudo -u nautobot /opt/nautobot/bin/pip install -r /opt/nautobot/.nautobot/reqs
+
+ln -sfv /opt/nautobot/.nautobot/.bashrc /opt/nautobot/.bashrc
