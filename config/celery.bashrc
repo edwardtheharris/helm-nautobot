82a9606b4aa2f402ce68150dac95e0c8e63b352b
Xander Harris <xandertheharris@gmail.com>
2024-08-09 10:25:10 -0700

    Update celery
    Closes #21

--- config/celery.bashrc
+++ config/celery.bashrc
@@ -7,8 +7,3 @@ PATH="/opt/celery/bin:$PATH"
 export HOME
 export NAUTOBOT_ROOT
 export PATH
-
-/usr/local/bin/python -m venv /opt/celery/
-/opt/celery/bin/pip install -r /opt/celery/.celery/reqs
-/opt/celery/bin/pip install -U pip
-/opt/celery/bin/nautobot-server celery worker --loglevel DEBUG
--- config/celery.init.sh
+++ config/celery.init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rfv /opt/celery
+mkdir -pv /opt/celery
 chown -v celery:celery /opt/celery
 
 apk add --no-cache sudo
@@ -8,4 +10,10 @@ sudo -u celery python -m venv /opt/celery
 
 sudo -u celery /opt/celery/bin/pip install -U pip
 
-sudo -u celery /opt/celery/bin/pip install -r /opt/celery/.celery/reqs
+sudo -u celery /opt/celery/bin/pip install -r /opt/.celery/reqs
+
+cat /opt/.celery/.bashrc > /opt/celery/.bashrc
+
+chown -v celery:celery /opt/celery/.bashrc
+
+sudo -E -u celery /opt/celery/bin/nautobot-server init
