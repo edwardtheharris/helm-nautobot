8756ead398fca52861070e2766d46c8e36dedbbc
Xander Harris <xandertheharris@gmail.com>
2024-08-14 22:31:31 -0700

    Switch to UWSGI service
    Closes #27

--- config/nautobot.init.sh
+++ config/nautobot.init.sh
@@ -19,8 +19,10 @@ sudo -u nautobot /opt/nautobot/bin/pip install --no-cache-dir -U pip
 sudo -u nautobot /opt/nautobot/bin/pip install --no-cache-dir -r /opt/.nautobot/reqs
 
 cat /opt/.nautobot/.bashrc > /opt/nautobot/.bashrc
+cat /opt/.nautobot/uwsgi.ini > /opt/nautobot/uwsgi.ini
 
 chown -v nautobot:nautobot /opt/nautobot/.bashrc
+chown -v nautobot:nautobot /opt/nautobot/uwsgi.ini
 
 NAUTOBOT_ROOT=/opt/nautobot
 
